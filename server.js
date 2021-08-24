235b7fae16db317f50d9109e4434dfde27455765
TylerLadas <tylerladas@hotmail.com>
2021-08-24 14:08:45 -0400

    edits to server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const path = require('path');
 
 // import from other files
 const sequelize = require("./config/connection");
