4ef087bd601c722d9c6972dccda2ac00bc91bae1
Jacob Banks <kobybanks@gmail.com>
2021-08-25 21:17:41 -0400

    added user routes, first pass at calandar

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,16 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
 
-
-router.get('/', (req, res) => {
-    res.render('home');
+router.get("/", (req, res) => {
+  res.render("home", { style: "style.css" });
 });
 
-router.get('/signup', (req, res) => {
-    res.render('signup');
+router.get("/signup", (req, res) => {
+  res.render("signup", { style: "style.css" });
 });
 
-router.get('/login', (req, res) => {
-    res.render('login');
+router.get("/login", (req, res) => {
+  res.render("login", { style: "style.css" });
 });
 
 module.exports = router;
