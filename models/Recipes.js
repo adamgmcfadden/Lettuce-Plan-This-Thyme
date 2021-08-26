61772699aaeb1afcf4de39ff28bab04b2ca41ffb
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-08-25 20:27:00 -0400

    completed models, associations and recipes.handlebars - all preliminary

---
+++ models/Recipes.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Recipes extends Model {}
+// create fields/columns for Post model
+Recipes.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    nutrition: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    servings: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    cook_time: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "recipes",
+  }
+);
+
+module.exports = Recipes;
