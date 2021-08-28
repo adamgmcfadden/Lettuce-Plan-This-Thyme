97a9bf82b7536a215fac3e2c26a8e7314c0dbc58
TylerLadas <tylerladas@hotmail.com>
2021-08-28 13:46:13 -0400

    add card spacing

--- public/javascript/recipe-search.js
+++ public/javascript/recipe-search.js
@@ -1,13 +1,13 @@
 // my api
 // const apiKey = "ca7027577b24470592ca8275b05b47b3"
 // kt api
-const apiKey = "c0a5df971c94493aa3d688d219a0a28d"
+// const apiKey = "c0a5df971c94493aa3d688d219a0a28d"
 const url = "https://api.spoonacular.com/recipes/complexSearch?query="
 const url2 = "&addRecipeInformation=true&addRecipeNutrition=true&number=20&apiKey=c0a5df971c94493aa3d688d219a0a28d"
 
 function getRecipe(searchValue) {
 
-    console.log(searchValue)
+    // console.log(searchValue)
     fetch(url + searchValue + url2)
     
     .then (res => res.json())
@@ -18,35 +18,39 @@ function getRecipe(searchValue) {
 
          recipes.forEach(recipe => {
 
-            const recipeTitle = document.createElement('p');
+            const recipeCard = document.createElement('div');
+            recipeCard.className = "card recipe-card col-5";
+            recipesDiv.append(recipeCard) 
+
+            const recipeTitle = document.createElement('h3');
             recipeTitle.innerText = `${recipe.title}`;
-            recipesDiv.append(recipeTitle);
+            recipeCard.appendChild(recipeTitle);
 
             const recipeImage = document.createElement('img');
             recipeImage.src = `${recipe.image}`;
-            recipesDiv.append(recipeImage);
+            recipeCard.appendChild(recipeImage);
 
-            const recipeServings = document.createElement('p');
+            const recipeServings = document.createElement('li');
             recipeServings.innerText = `Serves: ${recipe.servings}`;
-            recipesDiv.append(recipeServings);
+            recipeCard.appendChild(recipeServings);
 
-            const recipeTime = document.createElement('p');
+            const recipeTime = document.createElement('li');
             recipeTime.innerText = `Ready in: ${recipe.readyInMinutes} minutes`
-            recipesDiv.append(recipeTime)
+            recipeCard.appendChild(recipeTime)
 
             const recipeURL = document.createElement('a');
             recipeURL.href = `${recipe.sourceUrl}`;
             recipeURL.target = `_blank`
             recipeURL.innerText = `Click her for recipe!`;
-            recipesDiv.append(recipeURL)
+            recipeCard.appendChild(recipeURL)
 
             const faveBtn = document.createElement('button');
             faveBtn.innerText = `Add to favorites`;
-            recipesDiv.append(faveBtn);
+            recipeCard.appendChild(faveBtn);
 
             const calendarBtn = document.createElement('button');
             calendarBtn.innerText = 'Add to calendar';
-            recipesDiv.append(calendarBtn);
+            recipeCard.appendChild(calendarBtn);
              
         });
     }
@@ -58,27 +62,5 @@ document.querySelector('.search-form').addEventListener('submit', function(event
     const searchValue = document.querySelector('.search-bar').value.trim();
     getRecipe(searchValue);
     document.querySelector('.search-bar').value = "";
-
 });
 
-      // const data = {
-        //     recipe: res.results,
-        // };
-        // recipeArray = res.results
-        // recipeArray.forEach(element => {
-        //     console.log(element)
-
-            // const data = { searchValue };
-    // const options = {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(data)
-    // };
-    // fetch('/recipeSearch', options)
-    // .then(response => {
-    //     console.log(response)
-    // })
-
-    // document.location.replace('/recipes'); 
