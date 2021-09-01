7255931843854c2f74f991883de1aec5ab7016ff
Jacob Banks <kobybanks@gmail.com>
2021-09-01 13:44:14 -0400

    new calendar model, setting up for add to calendar button

--- controllers/calandar-routes.js
+++ controllers/calandar-routes.js
@@ -1,9 +1,57 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
 const withAuth = require("../utils/auth");
-
+const Calendar = require("../models/Calendar");
 router.get("/", withAuth, (req, res) => {
   res.render("calandar", { style: "calandar.css" });
+});
+router.get("/all", (req, res) => {
+  Calendar.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: [
+      "id",
+      "title",
+      "summary",
+      "year",
+      "month",
+      "day",
+      "meal",
+
+      "cook_time",
+    ],
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+      console.log(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+router.post("/", (req, res) => {
+  Calendar.create({
+    year: req.body.year,
+    month: req.body.month,
+    day: req.body.day,
+    title: req.body.title,
+    meal: req.body.meal,
+    cook_time: req.body.cook_time,
+    summary: req.body.summary,
+
+    user_id: req.session.user_id,
+  })
+    .then((dbPostData) => res.render("calandar", { data: req.body }))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
